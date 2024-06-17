114625a031a6dba1b11a2ce9d5ff7aea370c8338
Daniel Guirguis <155399947+Danny-Guirguis@users.noreply.github.com>
2024-06-17 16:34:01 +0300

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,9 +1,39 @@
-#include<iostream>
+#include <iostream>
+#include <random>
 using namespace std;
+
+typedef unsigned int uint;
+
+void guess(uint random_num);
 
 int main(){
 
-cout<<"Hello World!";
+    random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<mt19937::result_type>dist10(1, 10);
 
-return 0;
+    uint num = dist10(rng);
+
+    guess(num);
+
+    return 0;
+}
+
+void guess(uint random_num){
+
+    cout << "Guess the number from 1 to 10:";
+    uint guessed_num;
+    cin >> guessed_num;
+
+    if (guessed_num > random_num){
+        cout << "\nThe guessed number is too high, try again\n\n";
+        return guess(random_num);
+    }
+    else if (guessed_num < random_num){
+        cout << "\nThe guessed number is too low, try again\n\n";
+        return guess(random_num);
+    }
+    else{
+        cout << "Bravo! you guessed the number correctly";
+    }
 }
